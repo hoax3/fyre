98431ce781b3c21da7bf9018519debba0abdad77
Justin <65579812+hoax3@users.noreply.github.com>
2021-09-05 00:42:47 -0400

    Update adlist.py
    updated file paths to call dedicated file path.

--- dnsmasq/bin/adlist.py
+++ dnsmasq/bin/adlist.py
@@ -8,25 +8,25 @@ import requests
 
 # multiple adlists added to initial.txt 
 def multi_list():
-    adlist= open('./initial.txt').read().splitlines()
+    adlist= open('../lists/initial.txt').read().splitlines()
     for ad in adlist:
         r=requests.get(ad)
         status = r.status_code
         if status!=200:
             print("[%s] : Unable to pull from %s" % (status,ad))
         else:
-            with open("00-initial.txt","wb") as f:
+            with open("../lists/00-initial.txt","wb") as f:
                 f.write(r.content)
 
 # If no additions have been added to initial.txt                
 def initial_list():
-    adlist=open('./initial.txt',"r").read().splitlines()
+    adlist=open('../lists/initial.txt',"r").read().splitlines()
     for ad in adlist:
         r=requests.get()
-        with open("00-initial.txt","w") as f:
+        with open("../lists/00-initial.txt","w") as f:
             f.write(response.text)
 
-num_lines = sum(1 for line in open('./initial.txt'))
+num_lines = sum(1 for line in open('../lists/initial.txt'))
 
 if num_lines>1:
     multi_list()
