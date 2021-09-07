6cac16c7548d08f374b049f238ee7399a55802ea
Justin <65579812+hoax3@users.noreply.github.com>
2021-09-07 14:13:12 -0400

    Update adlist.py
    Added error handling, logging info, and minimized to single function.

--- dnsmasq/bin/adlist.py
+++ dnsmasq/bin/adlist.py
@@ -1,36 +1,43 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 # Cron set once a week to pull lists for adblocking
 # initial list is referencing:
 # https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
 import requests
+import logging
 
-# multiple adlists added to initial.txt 
-def multi_list():
-    adlist= open('../lib/initial.txt').read().splitlines()
-    for ad in adlist:
-        r=requests.get(ad)
-        status = r.status_code
-        if status!=200:
-            print("[%s] : Unable to pull from %s" % (status,ad))
-        else:
-            with open("../lib/00-initial.txt","wb") as f:
-                f.write(r.content)
+logging.basicConfig(
+        filename="/Users/justin/fyre-main/dnsmasq/log/adlistout.log", 
+        format="%(asctime)s  %(levelname)s  %(message)s",
+        level=logging.INFO)
 
-# If no additions have been added to initial.txt                
-def initial_list():
-    adlist=open('../lib/initial.txt',"r").read().splitlines()
-    for ad in adlist:
-        r=requests.get()
-        with open("../lib/00-initial.txt","w") as f:
-            f.write(response.text)
+def main():
+    logging.info("Starting Adlist Collection....")
+    adlist=open('../lib/initial.txt').read().splitlines()
+    num_lines = sum(1 for line in open('../lib/initial.txt'))
+    if num_lines > 1: 
+        logging.info("Additional lists added. Starting collection...")
+        for ad in adlist:
+            try:
+                r = requests.get(ad, timeout=3)
+            except requests.exceptions.RequestException as err:
+                logging.error("Unable to collect: %s:  %s" % (ad,err))
+            else:
+                logging.info("Successfully collected: %s" % ad)
+                with open("../lib/00-initial.txt","wb") as f:
+                    f.write(r.content)
+    else:
+        logging.info("Zero additions added to adlist, updating initial list")
+        for ad in adlist:
+            try:
+                r = requests.get(ad, timeout=3)
+            except requests.exceptions.RequestException as err:
+                logging.error("Unable to collect %s:  %s" % (ad,err))
+            else:
+                logging.info("Successfully collected: %s" % ad)
+                with open("../lib/00-initial.txt","wb") as f:
+                    f.write(r.content)
 
-num_lines = sum(1 for line in open('../lib/initial.txt'))
-
-if num_lines>1:
-    multi_list()
-else:
-    initial_list()
-
-    
+if __name__ == "__main__":
+    main()
